58bec649f0d33e661d5a9e2e06fa7b105d9e7586
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-08-12 21:46:05 +0530

    "feat: Implemented Customer,Dish,Restaurant,Order Items"

---
+++ src/com/hosanna/foodiecliapp/model/Customer.java
@@ -0,0 +1,91 @@
+package com.hosanna.foodiecliapp.model;
+
+import java.util.Objects;
+
+public class Customer {
+
+     /*
+    add the following properties
+    --------------------------------------
+    Datatype                  variable
+    --------------------------------------
+    String                      id
+    String                      name
+    String                      email
+    String                      password
+     */
+
+    /*
+    1. All the fields should be private
+    2. Create only no-arg constructor
+    3. Create Getters and Setter methods
+    4. Override hashCode() and equals() methods
+    5. Override toString() methods
+     */
+ private String id;
+ private String name;
+ private String email;
+ private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public Customer setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Customer setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Customer setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Customer setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Customer() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(password, customer.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
