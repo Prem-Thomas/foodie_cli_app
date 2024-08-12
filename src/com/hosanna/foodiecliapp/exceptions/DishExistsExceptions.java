4169335e348f74c30dcf6c8669987f6ac9197ec1
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-08-12 22:07:54 +0530

    "refactor: Refactored Methods"

---
+++ src/com/hosanna/foodiecliapp/exceptions/DishExistsExceptions.java
@@ -0,0 +1,8 @@
+package com.hosanna.foodiecliapp.exceptions;
+
+public class DishExistsExceptions extends Exception{
+
+    public DishExistsExceptions(String message) {
+        super(message);
+    }
+}
